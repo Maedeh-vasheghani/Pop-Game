9f83aab4024db9b67883aa99baf65e63495937d0
Maedeh-vasheghani <78743849+Maedeh-vasheghani@users.noreply.github.com>
2021-03-07 13:42:59 +0100

    Add files
    CSS, HTML files

---
+++ overlay.js
@@ -0,0 +1,6 @@
+// const btnScroll = document.querySelector("button");
+// const overlay1 = document.querySelector(".overlay-1");
+
+// btnScroll.addEventListener("click", () => {
+//     overlay1.style.transform = "translateY(-100vh)";
+// });
